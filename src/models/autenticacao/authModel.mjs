0802a581281e77e1b6d846b916a9dd8591fd2210
Gideone Lacerda Costa <89115386+GidLC@users.noreply.github.com>
2025-03-24 14:21:23 -0300

    Update authModel.mjs

--- src/models/autenticacao/authModel.mjs
+++ src/models/autenticacao/authModel.mjs
@@ -64,12 +64,12 @@ class AuthModel {
       INSERT INTO categoria_tr (nome, tipo, cor, icone, casal) VALUES("Roupas e Acessórios", 0, 8, 33, ?);
       INSERT INTO categoria_tr (nome, tipo, cor, icone, casal) VALUES("Água/Luz/Internet", 0, 9, 39, ?);
       INSERT INTO categoria_tr (nome, tipo, cor, icone, casal) VALUES("Despesas Diversas", 0, 10, 36, ?);
-      INSERT INTO categoria_tr (nome, tipo, cor, icone, casal) VALUES("*Ajuste*",0, 2, 36, ?);
+      INSERT INTO categoria_tr (nome, tipo, cor, icone, casal, cat_sistema) VALUES("*Ajuste*",0, 2, 36, ?, 1);
       INSERT INTO categoria_tr (nome, tipo, cor, icone, casal) VALUES("Salário", 1, 11, 38, ?);
       INSERT INTO categoria_tr (nome, tipo, cor, icone, casal) VALUES("Rendimentos", 1, 12, 37, ?);
       INSERT INTO categoria_tr (nome, tipo, cor, icone, casal) VALUES("Presentes", 1, 13, 26, ?);
       INSERT INTO categoria_tr (nome, tipo, cor, icone, casal) VALUES("Receitas Diversas", 1, 14, 31, ?);
-      INSERT INTO categoria_tr (nome, tipo, cor, icone, casal) VALUES("*Ajuste*",1, 3, 37, ?);
+      INSERT INTO categoria_tr (nome, tipo, cor, icone, casal, cat_sistema) VALUES("*Ajuste*",1, 3, 37, ?, 1);
       `;
 
       const queries = queryCategoria.split(';').filter(query => query.trim() !== '');
