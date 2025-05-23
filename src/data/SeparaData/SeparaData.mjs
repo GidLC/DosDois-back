443c55c9bdbd68c9641d8188693d07b2f47c405c
Gideone Lacerda Costa <89115386+GidLC@users.noreply.github.com>
2025-05-23 09:03:38 -0300

    Update SeparaData.mjs

--- src/data/SeparaData/SeparaData.mjs
+++ src/data/SeparaData/SeparaData.mjs
@@ -1,11 +1,11 @@
-import { DateTime } from 'luxon'
+﻿import { DateTime } from 'luxon'
 
 const separaData = async (data) => {
     const dateBRT = DateTime.fromISO(data, { zone: 'utc' }).setZone('America/Sao_Paulo')
 
     return {
         ano: dateBRT.year,
-        mes: dateBRT.month, // Ajuste para índice zero
+        mes: dateBRT.month - 1,
         dia: dateBRT.day,
         hora: dateBRT.hour,
         minuto: dateBRT.minute,
